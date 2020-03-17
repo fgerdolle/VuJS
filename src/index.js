8b35e11dc1167f24020ee0aaf24f999e0f359d47
Franck Gerdolle <franck.gerdolle@gmail.com>
2020-03-17 16:15:02 +0100

    Stade 3

--- src/index.js
+++ src/index.js
@@ -10,7 +10,14 @@ let vueInstance = new Vue({
         imageSrc: 'img/dta.png',
         firstName: "Franck",
         lastName: "Gerdolle",
-        counter: 0
+        counter: 0,
+        randomNumber: 1,
+        items: [
+            {id: 1, label: 'Nadochi'},
+            {id: 2, label: 'Pistolet'},
+            {id: 3, label: 'Fusil à levier'},
+            {id: 4, label: 'Armure Samurai'},
+        ]
     },
     created: function(){
         console.log("instance created");
@@ -27,11 +34,22 @@ let vueInstance = new Vue({
         },
         deburredMessage: function() {
             return _.deburr(this.message);
+        },
+        noContentMessage: function() {
+            return 'No content displayed, because random number genetared was : ' + this.randomNumber;
         }
     },
     methods: {
         increment: function() {
             this.counter++;
+        },
+        displayContent: function() {
+
+            this.randomNumber = _.random(1, 10);
+
+            console.log(this.randomNumber);
+
+            return this.randomNumber % 2;
         }
     },
     
