94e162364154a0a71d70247eceb2071fba944d1b
Franck Gerdolle <franck.gerdolle@gmail.com>
2020-03-17 14:34:50 +0100

    Stade 2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import _ from 'lodash';
 
 let vueInstance = new Vue({
     el: '#app',
@@ -8,17 +9,30 @@ let vueInstance = new Vue({
         textMessage: '2 < 3, isn\'t it ?',
         imageSrc: 'img/dta.png',
         firstName: "Franck",
-        lastName: "Gerdolle"
-    },
-    computed :{
-        fullName: function(){
-            return (this.firstName + ' ' + this.lastName);
-        }
+        lastName: "Gerdolle",
+        counter: 0
     },
     created: function(){
         console.log("instance created");
     },
     mounted:function(){
         console.log("instance mounted");
-    }
+    },
+    computed :{
+        fullName: function(){
+            return (this.firstName + ' ' + this.lastName);
+        },
+        reversedMessage: function() {
+            return this.message.split('').reverse().join('')
+        },
+        deburredMessage: function() {
+            return _.deburr(this.message);
+        }
+    },
+    methods: {
+        increment: function() {
+            this.counter++;
+        }
+    },
+    
 });
