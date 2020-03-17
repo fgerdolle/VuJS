92fcfe561212c65a4c630257b3f9d4752b556f01
Franck Gerdolle <franck.gerdolle@gmail.com>
2020-03-17 11:18:46 +0100

    stade 1 : advanced interpellation

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import Vue from 'vue';
 let vueInstance = new Vue({
     el: '#app',
     data: {
-        message: 'Hello from the End of The World !'
+        message: 'Hello from the End of The World !',
+        rawHtmlMessage: '<small>(Stay tuned)</small>',
+        textMessage: '2 < 3, isn\'t it ?',
+        imageSrc: 'img/dta.png'
+    },
+    created: function(){
+        console.log("instance created");
+    },
+    mounted:function(){
+        console.log("instance mounted");
     }
 });
