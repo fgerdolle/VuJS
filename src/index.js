fdf818fc9c444b6db503068025313dddf254d3a6
Franck Gerdolle <franck.gerdolle@gmail.com>
2020-03-17 12:01:50 +0100

    stade 1 : computed

--- src/index.js
+++ src/index.js
@@ -6,7 +6,14 @@ let vueInstance = new Vue({
         message: 'Hello from the End of The World !',
         rawHtmlMessage: '<small>(Stay tuned)</small>',
         textMessage: '2 < 3, isn\'t it ?',
-        imageSrc: 'img/dta.png'
+        imageSrc: 'img/dta.png',
+        firstName: "Franck",
+        lastName: "Gerdolle"
+    },
+    computed :{
+        fullName: function(){
+            return (this.firstName + ' ' + this.lastName);
+        }
     },
     created: function(){
         console.log("instance created");
